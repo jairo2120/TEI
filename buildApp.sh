ebfc395d361f6832c3d4c0dd89ab73423810e5b2
Jairo <jacarvajao@eafit.edu.co>
2021-04-16 22:01:11 -0500

    app1

---
+++ buildApp.sh
@@ -0,0 +1,13 @@
+mkdir app1
+chmod 777 app1
+cd app1 
+touch index.js
+
+apt-get update
+npm init --yes #starting the project
+
+#dependencies 
+npm i express morgan
+npm i nodemon -D
+npm i underscore 
+npm install mongoose 
